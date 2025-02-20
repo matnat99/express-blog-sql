4b42a71ea8c1ff2fc480b37068ebdc82defb675a
Mattia Natale <mattianatale07@gmail.com>
2025-02-20 17:06:56 +0100

    feat: add bonus

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,18 +20,33 @@ const show = (req, res) => {
 
   const postSql = "SELECT * FROM posts WHERE id = ?";
 
-  connection.query(postSql, [id], (err, results) => {
+  const tagsSql = `
+                   SELECT tags.*
+                   FROM tags
+                   JOIN post_tag ON post_tag.tag_id = tags.id 
+                   WHERE post_tag.post_id = ?
+                 `;
+
+  connection.query(postSql, [id], (err, postResults) => {
     if (err)
       return res.status(500).json({
         error: "Database query failed",
       });
 
-    if (results.length === 0)
+    if (postResults.length === 0)
       return res.status(404).json({
         error: "Post not found",
       });
 
-    res.json(results[0]);
+    const post = postResults[0];
+
+    connection.query(tagsSql, [id], (err, tagsResults) => {
+      if (err) return res.status(500).json({ error: "Database query failed" });
+
+      post.tags = tagsResults;
+
+      res.json(post);
+    });
   });
 };
 
